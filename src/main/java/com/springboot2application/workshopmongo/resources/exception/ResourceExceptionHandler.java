f0fc1af1a0631e36297205fc0833df96c91930bd
“danielmarianii” <danielmariani10@hotmail.com>
2022-12-10 23:43:31 -0300

    get user by id

---
+++ src/main/java/com/springboot2application/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.springboot2application.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.springboot2application.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError standardError = new StandardError(System.currentTimeMillis(), status.value(), "Not found",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+}
