86c56ca9747d7f8c230f53be6045e8387ce90b0f
“danielmarianii” <danielmariani10@hotmail.com>
2022-12-11 14:18:24 -0300

    insert user with POST

--- src/main/java/com/springboot2application/workshopmongo/services/UserService.java
+++ src/main/java/com/springboot2application/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot2application.workshopmongo.domain.User;
+import com.springboot2application.workshopmongo.dto.UserDTO;
 import com.springboot2application.workshopmongo.repository.UserRepository;
 import com.springboot2application.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
