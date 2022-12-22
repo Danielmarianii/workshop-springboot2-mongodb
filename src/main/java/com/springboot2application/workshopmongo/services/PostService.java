cf3a4941142dafc2287aa3d4c59b2086488c41ac
“danielmarianii” <danielmariani10@hotmail.com>
2022-12-22 02:05:03 -0300

    custom search

--- src/main/java/com/springboot2application/workshopmongo/services/PostService.java
+++ src/main/java/com/springboot2application/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.springboot2application.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return postRepository.searchTitle(text);
+	}
+	
+	public List<Post> customSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 + 60 + 60 + 1000);
+		return postRepository.customSearch(text, minDate, maxDate);
 	}
 }
